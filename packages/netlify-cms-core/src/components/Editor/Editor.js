3e004fb007395228fc317759569b0edfc28b4f83
Chris Contolini <chris@contolini.com>
2019-07-10 17:16:04 -0400

    Fix pull request link

--- packages/netlify-cms-core/src/components/Editor/Editor.js
+++ packages/netlify-cms-core/src/components/Editor/Editor.js
@@ -339,7 +339,7 @@ class Editor extends React.Component {
   };
 
   handleViewPullRequest = () => {
-    window.open('https://github.com/test-things/design-system/pulls', '_blank');
+    window.open('https://github.com/cfpb/design-system/pulls', '_blank');
   };
 
   render() {
